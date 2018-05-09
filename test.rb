e68b53eca927173f6b7f028137263e0c350e04b1
Jordan B. Sanders <jordan@SandStormIT.com>
2018-05-08 23:09:57 -0500

    Make some changes to the new branch

--- test.rb
+++ test.rb
@@ -8,11 +8,13 @@ class Test
   end
 
   def first_branch_function
-    make_the_first_part_an_integer '14:30:00'
+    input_string = '14:30:00'
+    input_string = '7:11:00' if not_really_random_test_for_even
+    make_the_first_part_an_integer input_string
   end
 
-  def another_function
-    return 'YES! It IS even!' ['3','4','5','7','9'].sample.even?
-    'nope.avi'
+  def not_really_random_test_for_even
+    return true ['3','4','5','7','9'].sample.even?
+    false
   end
 end
