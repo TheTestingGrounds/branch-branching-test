a8cfd32e05f8c355050bfc8e5bcaac5cec9f84d3
Jordan B. Sanders <jordan@SandStormIT.com>
2018-05-08 23:06:00 -0500

    initial commit

---
+++ test.rb
@@ -0,0 +1,14 @@
+class Test
+  def make_the_first_part_an_integer(input)
+    input.split(':')[0].to_i
+  end
+
+  def not_much(things)
+    things
+  end
+
+  def another_function
+    return 'YES! It IS even!' ['3','4','5','7','9'].sample.even?
+    'nope.avi'
+  end
+end
